aca065c7797af15838df30ae1b2d5119dee56918
LittleFish33 <3478628675@qq.com>
2021-05-03 08:11:56 -0700

    tests/loggen: enhancement, add functional test for reconnect opt
    Signed-off-by: LittleFish33 <3478628675@qq.com>

--- tests/python_functional/src/driver_io/network/network_io.py
+++ tests/python_functional/src/driver_io/network/network_io.py
@@ -21,6 +21,7 @@
 #
 #############################################################################
 from enum import Enum
+
 from pathlib2 import Path
 
 import src.testcase_parameters.testcase_parameters as tc_parameters
